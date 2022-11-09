41f24918e41d382ae7abd74a7ccdbdb090d19995
Ericawanja <wanjaerica@gmail.com>
2022-11-09 08:57:14 +0300

    reset

--- redux toolkit demo/rtk_demo/src/Components/Counter.jsx
+++ redux toolkit demo/rtk_demo/src/Components/Counter.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import { decrement, increment } from './Redux/features/Counter/CounterSlice'
+import React from "react";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  reset,
+  add_by
+} from "./Redux/features/Counter/CounterSlice";
 
 function Counter() {
-    const counter = useSelector((state)=>state.counter.count)
-    const dispatch = useDispatch()
-  
+  const counter = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  let [amount, set_amount] = useState('');
+
   return (
     <div>
-        <h1>{counter}</h1>
-        <button  onClick={()=> dispatch(increment())}>+</button>
-        <button onClick ={()=> dispatch(decrement())}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+      <input
+        type="text"
+        value={amount}
+        placeholder="Enter amount to add"
+        onChange={(e) => set_amount(e.target.value)}
+      />
+      <button onClick={() => dispatch(add_by(amount))}>Add</button>
     </div>
-  )
+  );
 }
 
-export default Counter
+export default Counter;
--- redux toolkit demo/rtk_demo/src/Components/Redux/features/Counter/CounterSlice.jsx
+++ redux toolkit demo/rtk_demo/src/Components/Redux/features/Counter/CounterSlice.jsx
@@ -1,21 +1,29 @@
-import {createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    count:0
-}
+  count: 0,
+};
 
 export const counterSlice = createSlice({
-    name:'counter',
-    initialState,
-    reducers:{
-        increment:(state)=>{
-            state.count += 1;
-        },
-        decrement:(state)=> {state.count-=1}
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0
+    },
+    add_by:(state, action)=>{
+        console.log(action)
+    state.count= state.count + +action.payload
     }
+  },
+});
 
-})
-
-export const {increment, decrement} = counterSlice.actions
-console.log(counterSlice.reducer);
-export default counterSlice.reducer
+export const { increment, decrement, reset, add_by } = counterSlice.actions;
+console.log(counterSlice);
+export default counterSlice.reducer;
