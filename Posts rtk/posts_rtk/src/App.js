e0aa7bc3fef6d054c3194e7ddd4470356acfe73e
Ericawanja <wanjaerica@gmail.com>
2022-11-09 11:53:27 +0300

    rtk app

---
+++ Posts rtk/posts_rtk/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PostList from "./components/PostList";
+import "./components/Styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <PostList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
